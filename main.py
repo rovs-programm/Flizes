cea597b7cc6b22915f2386bc1271bb986302a38d
rovsprogramm <rovs.informatika@gmail.com>
2020-11-27 09:56:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import math
+#Fitsful we need plitka
+print("Input storoni odnoj plitki, plitki budet 84 ")
+a=int(input("Input 1st side: "))
+b=int(input("Input 2nd side: "))
+daudzums=84
+S=a*b
+print("Laukums 1 plitki = " ,S)
+SS=S*daudzums
+print("Laukums vsego pola = " ,SS)
+
+
+print("Input storoni odnoi oboi, oboev budet 4 ")
+a1=int(input("Input 1st side: "))
+b1=int(input("Input 2nd side: "))
+daudzums1=4
+S1=a1*b1
+print("Laukums 1 oboi = " ,S1)
+SS1=S1*daudzums1
+print("Laukums vsego oboev = " ,SS1)
+
+
+'''print("Input storoni odnoi oboi, oboev budet 4 ")
+a1=int(input("Input 1st side: "))
+b1=int(input("Input 2nd side: "))
+daudzums1=4
+S1=a1*b1
+print("Laukums 1 oboi = " ,S1)
+SS1=S1*daudzums1
+print("Laukums vsego oboev = " ,SS1)'''
